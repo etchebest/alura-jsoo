37d84728aea62627b898e8b2b523c50a540c15e9
cristian.etchebest <cristian.etchebest@meta.com.br>
2022-02-18 13:22:03 -0300

    Criando classes funcionarios

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 import { Cliente } from "./Cliente.js";
-import { Conta } from "./Conta.js";
-import { ContaCorrente } from "./ContaCorrente.js";
-import { ContaPoupanca } from "./ContaPoupanca.js";
-import { ContaSalario } from "./ContaSalaraio.js";
+import { ContaCorrente } from "./Conta/ContaCorrente.js";
+import { ContaPoupanca } from "./Conta/ContaPoupanca.js";
+import { ContaSalario } from "./Conta/ContaSalaraio.js";
 
 const cliente1 = new Cliente("Cristian", 92976905053);
-const ccCristian = new ContaCorrente(cliente1, 1001);
-ccCristian.depositar(500);
-ccCristian.sacar(50);
+const cc = new ContaCorrente(cliente1, 1001);
+cc.depositar(500);
+cc.sacar(50);
 
-const contaPoupanca = new ContaPoupanca(50, cliente1, 1001)
+const cp = new ContaPoupanca(50, cliente1, 1001);
+cp.depositar(500);
 
-const contaSalario = new ContaSalario(cliente1);
-contaSalario.depositar(100);
-contaSalario.sacar(30);
+const cs = new ContaSalario(cliente1);
+cs.depositar(100);
+cs.sacar(30);
 
-console.log(contaSalario);
+console.log(cliente1, cc, cp, cs);
